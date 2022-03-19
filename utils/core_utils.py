414510546977b8f4117b30f92e4043ea54bc68ab
Fanjie Kong <fk43@duke.edu>
2022-03-19 12:44:34 -0400

    Delete core_utils.py

--- utils/core_utils.py
+++
@@ -1,8 +0,0 @@
-def get_optim(model, args):
-    if args.opt == "adam":
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, weight_decay=args.reg)
-    elif args.opt == 'sgd':
-        optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr, momentum=0.9, weight_decay=args.reg)
-    else:
-        raise NotImplementedError
-    return optimizer
